8ac5c6203e9123d1fd9bd3072a40e12af625590b
macoyshev <make.oynoshev@gmail.com>
2022-02-20 16:43:50 +0300

    connection func fix

--- fsa.py
+++ fsa.py
@@ -1,67 +1,76 @@
 
+from ctypes import sizeof
+from math import fabs
+from operator import truediv
+from platform import node
+import re
+
+
 class Errors:
     output = "result.txt"
 
     @staticmethod    
-    def E1(state):
-        stream = open(Errors.output, "a")
+    def undefinedState(state):
+        stream = open(Errors.output, "w")
         stream.write(f"Error:\nE1: A state '{state}' is not in the set of states")
         stream.close()
         exit(0)
 
     @staticmethod  
-    def E2():
-        stream = open(Errors.output, "a")
+    def disjointStates():
+        stream = open(Errors.output, "w")
         stream.write("Error:\nE2: Some states are disjoint")
         stream.close()
         exit(0)
 
     @staticmethod  
-    def E3(trans):
-        stream = open(Errors.output, "a")
+    def undefinedTransion(trans):
+        stream = open(Errors.output, "w")
         stream.write(f"Error:\nE3: A transition '{trans}' is not represented in the alphabet")
         stream.close()
         exit(0)
 
     @staticmethod  
-    def E4():
-        stream = open(Errors.output, "a")
+    def undefinedUnitialState():
+        stream = open(Errors.output, "w")
         stream.write("Error:\nE4: Initial state is not defined")
         stream.close()
         exit(0)
 
     @staticmethod  
-    def E5():
-        stream = open(Errors.output, "a")
+    def malformedInput():
+        stream = open(Errors.output, "w")
         stream.write("Error:\nE5: Input file is malformed")
         stream.close()
         exit(0)
+
 
 class Warnings:
 
     output: str = "result.txt"
     
     @staticmethod
-    def W1():
+    def undefinedAcceptedState():
         stream = open(Errors.output, "a")
         stream.write("W1: Accepting state is not defined")
         stream.close()
         
     @staticmethod
-    def W2():
+    def unreachableStates():
         stream = open(Errors.output, "a")
         stream.write("W2: Some states are not reachable from the initial state")
         stream.close()
 
     @staticmethod
-    def W2():
+    def fsaNondeterministic():
         stream = open(Errors.output, "a")
         stream.write("W3: FSA is nondeterministic")
         stream.close()
 
+
 def parse(data: list):
     if (len(data) != 5):
-        Errors.E5()
+        Errors.malformedInput()
         
     data[0] = data[0].replace("states=","");
     data[1] = data[1].replace("alpha=", "");
@@ -79,31 +88,112 @@ def parse(data: list):
             
         if "," in data[i]:
             data[i] = data[i].split(",")
-        
-        if (len(data[i]) == 0):
-            data[i] = []
         else:
-            data[i] = data[i]
+            if (len(data[i]) == 0):
+                data[i] = []
+            else:
+                data[i] = [data[i]]
             
     
     return data;
 
 
-def main():
-    stream = open("result.txt", "w")
-    stream.close();
+def checkStates(states, inits, fins, trans):
+    #checking states in trans
+    for i in range(0, len(trans), 3):
+        if trans[i] not in states:
+            Errors.undefinedState(trans[i])
     
-    try:
-        stream = open("fsa.txt")
-        data = stream.readlines()
-        stream.close()
-    except:
-        Errors.E5()
+    #checking states in fins
+    for st in fins:
+        if st not in states:
+            Errors.undefinedState(st)
     
-    data = parse(data)
+    #checking states in inits
+    for st in inits:
+        if st not in states:
+            Errors.undefinedState(st)
+
+
+def checkInitState(inits):
+    if (len(inits) == 0):
+        Errors.undefinedUnitialState()
+
+
+def checkAlphabet(trans, alphas):
+    for i in range(1, len(trans), 3):
+        if trans[i] not in alphas:
+            Errors.undefinedTransion(trans[i])
+
+
+def checkFormat(data):
+    if (len(data) != 5):
+        Errors.malformedInput()
+
+
+def chechConnection(trans):
+    nodes = []
+    tree = []
     
-    statesAllowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    alphasAllowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
+    for i in range(0, len(trans), 3):
+        nodes.append(trans[i])
+        
+    for i in range(1, len(nodes), 2):
+        if (nodes[i] == nodes[i - 1]):
+            if (len(tree) == 0):
+                tree.append([nodes[i]])
+                continue
+                
+            if not isInAnyBranch(tree, nodes[i]):
+                tree.append([nodes[i]])
+        
+        else:
+            if (len(tree) == 0):
+                tree.append([nodes[i], nodes[i-1]])
+                continue
+        
+            index1 = getIndexOfBranch(nodes[i])
+            index2 = getIndexOfBranch(nodes[i - 1])
+            
+            if index1 != -1 and index2 != -1:
+                newBranch = tree[index1] + tree[index2]
+                tree.append(newBranch)
+                tree.pop(index1)
+                tree.pop(index2)
+                continue
+                
+            if index1 == -1 and index2 == -1:
+                tree.append([nodes[i], nodes[i-1]])
+                continue
+            
+            if index1 == -1:
+                tree[index2].append(nodes[i-1])
+            else:
+                tree[index2].append(nodes[i])
+                
+    if len(tree) != 1:
+        Errors.disjointStates()
+                
+
+def isInAnyBranch(tree, node):
+    for branch in tree:
+        if node in branch:
+            return True
+    
+    return False
+
+
+def getIndexOfBranch(tree, node):
+    for i in range(len(tree)):
+        if node in tree[i]:
+            return i
+        
+    return -1;
+
+
+def validate(data: list):
+    
+    checkFormat(data)
     
     states = data[0]
     alphas = data[1]
@@ -111,27 +201,24 @@ def main():
     fins = data[3]
     trans = data[4]
     
-    for st in states:
-        for s in st:
-            if s not in statesAllowedSymbols:
-                Errors.E5()
-                
-    for al in alphas:
-        for s in al: 
-            if s not in alphasAllowedSymbols:
-                Errors.E5()
+    checkStates(states, inits, fins, trans)
     
-    if (len(inits) == 0):
-        Errors.E4()
+    checkInitState(inits)
     
-    for i in range(1, len(trans), 3):
-        if trans[i] not in alphas:
-            Errors.E3(trans[i])
-    
-    for i in range(0, len(trans), 3):
-        if trans[i] not in states:
-            Errors.E1(trans[i])
-    
-main();
+    checkAlphabet(trans, alphas)
 
-asd
+
+
+    
+
+    
+
+def main():
+    stream = open("fsa.txt", "r")
+    data = stream.readlines()
+    stream.close()
+    
+    data = parse(data)
+    validate(data)
+
+main()
